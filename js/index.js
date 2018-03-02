776f2049187e1d9f13202fcef16fb26f8835a70b
MarjanSirovljevic <marjansirovljevic@gmail.com>
2018-03-02 15:28:32 +0100

    Adding basic react components - presentational for now

--- js/index.js
+++ js/index.js
@@ -1,23 +1,53 @@
 class App extends React.Component {
-
   render() {
-
     const jsx = (
       <div id="page">
-        <header id="header">HEADER</header>
+        <Header />
         <main id="main">
           <section id="top">TOP</section>
           <section id="center">
-            <aside id="gallery">GALLERY</aside>
-            <aside id="details" className="disabled">DETAILS</aside>
+            <Articles />
+            <ArticleDetails />
           </section>
           <section id="bottom">BOTTOM</section>
         </main>
-        <footer id="footer">FOOTER</footer>
+        <Footer />
       </div>
     );
-
     return jsx;
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header id="header">Header Component</header>
+    );
+  }
+}
+
+class Articles extends React.Component {
+  render() {
+    return (
+      <aside id="gallery">Articles Component</aside>
+    );
+  }
+}
+
+class ArticleDetails extends React.Component {
+  render() {
+    const article = this.props.selectedArticle;
+    return (
+      <aside id="details" className="disabled">Details Component</aside>
+    );
+  }
+}
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer id="footer">Footer Component</footer>
+    );
   }
 }
 
